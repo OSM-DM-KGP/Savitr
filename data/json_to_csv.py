06b87a94b614579a0eff7f2cd47c949d7d2af2f3
Kaustubh Hiware <hiwarekaustubh@googlemail.com>
2017-12-02 00:15:31 +0530

    Corrected roll number in report

--- data/json_to_csv.py
+++ data/json_to_csv.py
@@ -7,6 +7,14 @@ df = pd.read_json('all_tweets.json')
 #    data = json.load(f)
 # df = pd.DataFrame(data)
 
+df['cr'], df['acr'] = df['cr'].astype(str), df['acr'].astype(str)
+
+# convert {u'$date': u'actual'} to actual
+for i, row in df.iterrows():
+	cr1,acr1 = row.cr[13:-2], row.acr[13:-2]
+	df.set_value(i,'cr',cr1)
+	df.set_value(i,'acr',acr1)
+
 df.to_csv('data.csv',
     index=False,
     columns=['_id', 'lang', 'tln', 'cc','loc','p','tlt','plt','t','cr','acr','pln','flrs','f','uid'],
