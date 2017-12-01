4a17a105c6139c8ac2415bd1be7b2849b5d9497a
Kaustubh Hiware <hiwarekaustubh@googlemail.com>
2017-12-02 02:00:26 +0530

    Update all tweets. 2,39K tweets

--- data/json_to_csv.py
+++ data/json_to_csv.py
@@ -9,11 +9,23 @@ df = pd.read_json('all_tweets.json')
 
 df['cr'], df['acr'] = df['cr'].astype(str), df['acr'].astype(str)
 
+cr_new, acr_new = [], []
 # convert {u'$date': u'actual'} to actual
 for i, row in df.iterrows():
 	cr1,acr1 = row.cr[13:-2], row.acr[13:-2]
-	df.set_value(i,'cr',cr1)
-	df.set_value(i,'acr',acr1)
+	# df.set_value(i,'cr',cr1)
+	# df.set_value(i,'acr',acr1)
+	cr_new.append(cr1)
+	acr_new.append(acr1)
+	print i
+	if i %10000 == 0:
+		print 'out of', str(len(df))
+
+crname, acrname = df.columns[3], df.columns[1]
+df.drop(crname, axis = 1, inplace = True)
+df.drop(acrname, axis = 1, inplace = True)
+df[crname] = cr_new
+df[acrname] = acr_new
 
 df.to_csv('data.csv',
     index=False,
